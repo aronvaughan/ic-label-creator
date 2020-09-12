95366087381469506cd862db8ec9cd6fd4e7dd8c
Aron Vaughan <aronvaughan@outlook.com>
2020-09-12 11:48:09 -0500

    adding nixie specific chip labels and letting them be in  a separate file from the original library set

---
+++ nixie-chips.js
@@ -0,0 +1,58 @@
+//Create Overline Symbols: https://fsymbols.com/generators/overline/
+var nixieChips = {
+    
+    'IN-16' : {
+      description : 'IN-16 NIX-I',
+      type: 'nixie',
+      pins: {
+        1 : 'n/a',
+        2 : '0',
+        3 : '9',
+        4 : '8',
+        5 : ',',
+        6 : '2',
+        7 : '6',
+        8 : '5',
+        9 : '4',
+        10 : ',',
+        11 : '3',
+        12 : '7',
+        13 : '1',
+        14 : 'a->⏚'
+      },
+    },
+  
+  // empty template
+  ' ' : {
+    description : '',
+    type: '',
+    heightPins: 3,
+    pins: {
+      1 : '',
+      2 : '',
+      3 : '',
+      4 : '',
+      5 : '',
+      6 : '',
+      7 : '',
+      8 : '',
+      9 : '',
+      10: '',
+      11: '',
+      12: '',
+      13: '',
+      14: '',
+      15: '',
+      16: '',
+      17: '',
+      18: '',
+      19: '',
+      20: '',
+      21: '',
+      22: '',
+      23: '',
+      24: '',        
+    },
+  },    
+
+};  // end of var "chips"
